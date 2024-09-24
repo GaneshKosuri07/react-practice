60ff3deb74f2d1796e2754471dc4ffbc9a5de605
GaneshKosuri07 <147240479+GaneshKosuri07@users.noreply.github.com>
2024-09-24 10:14:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HelloWorld from './HelloWorld'//need to type
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+    </div>
+  )
+}
+
+export default App
